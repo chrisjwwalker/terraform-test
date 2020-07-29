e74cb18667d2a74afd312c367d42b721d2bb27b1
Chris J W Walker <8091191+chrisjwwalker@users.noreply.github.com>
2020-07-29 11:54:44 +0100

    Added custom scopes

--- modules/aws/cognito/resource-servers/main.tf
+++ modules/aws/cognito/resource-servers/main.tf
@@ -1,10 +1,10 @@
 resource "aws_cognito_resource_server" "ogd_resource_server" {
-  identifier = "https://example.com"
+  identifier = "ogd"
   name = "ogd_resource_server"
 
   scope {
     scope_description = "test scope"
-    scope_name = "ogd:test"
+    scope_name = "test"
   }
 
   user_pool_id = var.ogd_pool_id
