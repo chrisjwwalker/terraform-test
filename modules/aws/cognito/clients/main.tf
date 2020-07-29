97618704fafaa291ba647f93815266222575b32d
Chris J W Walker <8091191+chrisjwwalker@users.noreply.github.com>
2020-07-29 11:38:35 +0100

    Added auth code flow

--- modules/aws/cognito/clients/main.tf
+++ modules/aws/cognito/clients/main.tf
@@ -2,7 +2,7 @@ resource "aws_cognito_user_pool_client" "ogd_client_service" {
   name = "ogd-client-service"
   user_pool_id = var.ogd_pool_id
 
-  allowed_oauth_flows = ["client_credentials"]
+  allowed_oauth_flows = ["client_credentials", "code"]
   allowed_oauth_flows_user_pool_client = true
   allowed_oauth_scopes = ["aws.cognito.signin.user.admin"]
 
