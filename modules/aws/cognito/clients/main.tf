ede98df54e195d916bbd5f6f05e9cc59ce977975
Chris J W Walker <8091191+chrisjwwalker@users.noreply.github.com>
2020-07-29 11:26:05 +0100

    Added allowed oauth scopes

--- modules/aws/cognito/clients/main.tf
+++ modules/aws/cognito/clients/main.tf
@@ -4,6 +4,7 @@ resource "aws_cognito_user_pool_client" "ogd_client_service" {
 
   allowed_oauth_flows = ["client_credentials"]
   allowed_oauth_flows_user_pool_client = true
+  allowed_oauth_scopes = ["openid"]
 
   generate_secret = true
 }
